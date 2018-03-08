f8c600314a746ea00e6da3c4ed82fa89135915bb
Oleg979 <33599959+Oleg979@users.noreply.github.com>
2018-03-08 11:50:41 +0400

    Create Задача4.php

---
+++ Задача4.php
@@ -0,0 +1,28 @@
+<?php
+function packStr($str) {
+    $packed = "";
+    for($i=0; $i<strlen($str);$i++) {
+        $c = $str[$i]; $len = 1;
+        while($str[$i] == $str[$i+1]) {
+            $len++; $i++;
+        }
+        $packed .= $len.$str[$i];
+    }
+    return $packed;
+}
+
+function unpackStr($str) {
+    $unpacked = "";
+    for($i=0;$i<strlen($str);$i+=2)
+        for($j=0; $j<(int)$str[$i];$j++)
+            $unpacked.=$str[$i+1];
+    return $unpacked;
+}
+
+$str = "wwwwxxghiiooo";
+$packed = packStr($str);
+echo $packed." ";
+$unpacked = unpackStr($packed);
+echo $unpacked;
+
+?>
