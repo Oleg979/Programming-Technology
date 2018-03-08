f80a8cc412bde5f3becd286aac2dcd8a7290df4a
Oleg979 <33599959+Oleg979@users.noreply.github.com>
2018-03-08 20:17:43 +0400

    Update Задача4.php

--- Задача4.php
+++ Задача4.php
@@ -1,27 +1,27 @@
 <?php
 function packStr($str) {
-    $packed = "";
+    $packed = [];
     for($i=0; $i<strlen($str);$i++) {
         $c = $str[$i]; $len = 1;
         while($str[$i] == $str[$i+1]) {
             $len++; $i++;
         }
-        $packed .= $len.$str[$i];
+        array_push($packed, $len, $str[$i]);
     }
     return $packed;
 }
 
 function unpackStr($str) {
     $unpacked = "";
-    for($i=0;$i<strlen($str);$i+=2)
+    for($i=0;$i<count($str);$i+=2)
         for($j=0; $j<(int)$str[$i];$j++)
             $unpacked.=$str[$i+1];
     return $unpacked;
 }
 
-$str = "wwwwxxghiiooo";
+$str = "wwwwwwwwwwwwwxxghiioooooooooooooooooooooooo";
 $packed = packStr($str);
-echo $packed."\n";
+echo implode("",$packed)." ";
 $unpacked = unpackStr($packed);
 echo $unpacked;
 ?>
