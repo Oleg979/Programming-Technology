1085693d8ba6da463749a6eaf6fc08b867e3801b
Oleg979 <33599959+Oleg979@users.noreply.github.com>
2018-03-29 17:35:19 +0400

    Create Задача6.php

---
+++ Задача6.php
@@ -0,0 +1,29 @@
+<?php
+function addOne($str,$pos) {
+    $res = $str;
+    $res[$pos] = '1';
+    return $res;
+}
+
+function generate($n) {
+    global $res;
+    for($i=0;$i<$n; $i++) $tmp .= '0';
+    array_push($res,$tmp);
+    for($i=0;$i<$n;$i++) {
+        array_push($res,addOne($tmp,$i));
+        fill($i,$res[count($res)-1]);
+    }
+    return $res;
+}
+
+function fill($pos, $str) {
+    global $res,$n;
+    for($j=$pos+2;$j<$n;$j++) {
+        array_push($res,addOne($str,$j));
+        fill($j,$res[count($res)-1]);
+    }
+}
+
+$n = 7; $res = [];
+print_r(generate($n));
+?>
