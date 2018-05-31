ecf46d6b4f35faac4ada6143f86a1f3a51411312
Oleg979 <33599959+Oleg979@users.noreply.github.com>
2018-05-31 22:15:25 +0400

    Update Задача13.js

--- Задача13.js
+++ Задача13.js
@@ -1,12 +1,13 @@
 const reverse = n => {
   let res = [], idx = 1
+  
   do res.unshift(n % 2 ? 1 : 0)
   while((n = Math.floor(n / 2)) > 0)
+    
   return res.reduce((res, d) => {
     if (res != 0)idx *= 2
     return res + d * idx
   }, 0)
-  
 }
 
 for(let i = 512; i <= 1024; i++)
